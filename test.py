f40a1ed8bb0cabe074d7f00d7e9fdb5d4b4a2e0d
Lakshay Saini <93211383+WhitehatLaksh@users.noreply.github.com>
2022-10-27 20:31:54 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,45 +1,28 @@
-
-import unittest
+from unittest import TestCase
+from webbrowser import Chrome
+from selenium import webdriver 
 import time
-import os
-from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+
+
+
+driver = webdriver.Chrome(r"C:/Users/saini/OneDrive/Documents/project1/chromedriver.exe")
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(options=options)
+driver.maximize_window()  
+driver.maximize_window() 
+
+    
+if __name__ == '__main__': TestCase()
 
 
-#class FirstSampleTest(unittest.TestCase):
-    # Generate capabilites from here: https://www.lambdatest.com/capabilities-generator/
-    # setUp runs before each test case and
- ##      desired_caps = {
-   #         'LT:Options': {
-    #            "build": "Python Demo",  # Change your build name here
-     #           "name": "Python Demo Test",  # Change your test name here
-      #          "platformName": "Windows 11",
-       #         "selenium_version": "4.0.0",
-        #        "console": 'true',  # Enable or disable console logs
-         #       "network": 'true',  # Enable or disable network logs
-              
-          #  "browserName": "firefox",
-           # "browserVersion": "latest",
-        #}
+driver.get("https://unicreds.com/contact-us")  
+time.sleep(100)
+#driver.find_element_by_xpath('"//*[@id="exit_popup_close"]').click()
+time.sleep(100)
+#driver.find_element_by_xpath('//*[@id="zsiq_float"]/img').click()
+time.sleep(10000)
 
-
-
-def tearDown(self):
-        self.driver.quit()
-
-def test_demo_site(self):
-
-       
-        print('Loading URL')
-        driver.get("https://www.google.com")
-
-   #     driver.find_element(By.CLASS_NAME, "nav-action-inner" ).click()
-   #     driver.find_element(By.ID, "Email").sendkeys("lakshayindian19@live.com")
-           
-
-if __name__ == "__main__":
-    unittest.main()
